6aede37c5f1e3a3c058a44abb1e594232ba73021
unknown <agmarchena>
2023-03-13 16:14:50 +0100

    estructura del proyecto

---
+++ src/test/java/testExamen/TestExamenTests.java
@@ -0,0 +1,15 @@
+package testExamen;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestExamenTests {
+	
+	@Test()
+	public void checkClassConstantes() throws ClassNotFoundException {
+		assertTrue(1==1);
+	}
+	
+
+}
