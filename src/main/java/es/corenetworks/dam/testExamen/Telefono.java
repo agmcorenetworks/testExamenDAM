0d969cc564b01ddd92398bdcef46b9b7295a3e4b
dosaguz <xdg2001@gmail.com>
2023-03-13 21:12:33 +0100

    Simulacro de examen

--- src/main/java/es/corenetworks/dam/testExamen/Telefono.java
+++ src/main/java/es/corenetworks/dam/testExamen/Telefono.java
@@ -1,6 +1,7 @@
 package es.corenetworks.dam.testExamen;
 
 import es.corenetworks.dam.testExamen.utilidades.NumeroNoValidoException;
+import es.corenetworks.dam.testExamen.utilidades.NumeroNotFoundException;
 
 public class Telefono {
 
@@ -9,23 +10,30 @@ public class Telefono {
 			Agenda agenda1 = new Agenda();
 			try {
 				agenda1.añadirNumero(12345678);
-			} catch (NumeroNoValidoException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
 				agenda1.añadirNumero(87654321);
 			} catch (NumeroNoValidoException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			try {
-				System.out.println(agenda1.eliminarNumero(87654321));
+			/*try {
+				agenda1.añadirNumero(87654321);
 			} catch (NumeroNoValidoException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		/*	try {
+				System.out.println(agenda1.eliminarNumero(87654321));
+			} catch (NumeroNoValidoException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/
 			System.out.println(agenda1.getTamanyoAgenda());
+			try {
+				System.out.println(agenda1.buscarNumero(87654321));
+			} catch (NumeroNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 		}
 }
